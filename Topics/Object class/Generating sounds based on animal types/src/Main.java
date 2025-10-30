31f21c1d68f62162d9d59f56f06a4cd102ead478
DannyTran1806 <dannytran1806@gmail.com>
2025-10-30 07:29:46 +0100

    initialize database setu for step 3 of the project

--- Topics/Object class/Generating sounds based on animal types/src/Main.java
+++ Topics/Object class/Generating sounds based on animal types/src/Main.java
@@ -4,46 +4,21 @@ import java.util.*;
 public class Main {
 
     // Define Animal class here
-    public class Animal{
-        String name; 
-        public Animal (String name){
-            this.name = name; 
-        }
-        public String sound (){}
-    }
 
     // Define Dog class here
-    public class Dog extends Animal{
-        @Override
-        public String sound(){
-            System.out.println("Bark!"); 
-        }
-    }
 
     // Define Cat class here
-    public class Cat extends Animal{
-        @Override
-        public String sound(){
-            System.out.println("Meow!"); 
-        }
-    }
 
     public static void main(String[] args) {
 
         // In this section, you need to add code to receive input
         // and create the corresponding animal object
-        Scanner input = new Scanner(System.in); 
-        String animalType = input.nextLine(); 
-        Animal dog = new Dog(); 
-        Animal cat = new Cat(); 
-        
 
         // Replace the placeholders with the appropriate calls
-        if(animalType.equals("Dog")) {
-            dog.sound(); 
+        if(/*Dog?*/) {
 
-        } else if(animalType.equals("Cat")) {
-            cat.sound(); 
+        } else if(/*Cat?*/) {
+
         } else {
             System.out.println("Animal type not recognized");
         }
